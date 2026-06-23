011ebd8ef2ac87151209fbfbcda91f3bbad40222
VitorB-Fetal <vitorfetal.dev@gmail.com>
2026-06-23 13:23:21 -0300

    feat: insert rows with execute JDBC

--- src/main/java/com/prisma/java_fx/JavaFxApplication.java
+++ src/main/java/com/prisma/java_fx/JavaFxApplication.java
@@ -1,18 +1,18 @@
 package com.prisma.java_fx;
 
+import java.sql.ResultSet;
 import db.DB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @SpringBootApplication
 public class JavaFxApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 
 		SpringApplication.run(JavaFxApplication.class, args);
 
@@ -43,6 +43,52 @@ public class JavaFxApplication {
 			DB.closeConnection(conn);
 		}
 
-	}
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+		Connection conn2 = null;
+		PreparedStatement st2 = null;
+		rs = null;
+		try {
+			conn2 = DB.getConnection();
+
+			st2 = conn2.prepareStatement(
+							"INSERT INTO seller " +
+							"(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+									"VALUES" +
+									"(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			st2.setString(1, "Carl Purple");
+			st2.setString(2, "Carl@gmail.com");
+			st2.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
+			st2.setDouble(4, 3000.00);
+			st2.setInt(5, 4);
+
+
+			int rowsAffected = st2.executeUpdate();
+			if (rowsAffected > 0 )
+			{
+				System.out.println("Done! Rows affected: " + rowsAffected);
+				rs = st2.getGeneratedKeys();
+				while (rs.next())
+				{
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " +  id);
+				}
+			}
+				System.out.println("Not affected new rows");
+
+
+
+		} catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+		finally {
+			DB.closeStatement(st2);
+			DB.closeResultSet(rs);
+			DB.closeConnection(conn2);
+
+		}
+
+    }
 
 }
